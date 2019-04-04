718cbf2f892f170afcb618faf52de799f5e60d37
CUMTER <1451396178@qq.com>
2019-04-04 16:26:39 +0800

    跳到系统选择动态壁纸页面
    Signed-off-by: cumters <1487748886@qq.com>

--- LiuWallpaperManager/src/main/java/com/liutong/liuwallpapermanager/adapter/WallpaperInfoAdapter.java
+++ LiuWallpaperManager/src/main/java/com/liutong/liuwallpapermanager/adapter/WallpaperInfoAdapter.java
@@ -1,16 +1,21 @@
 package com.liutong.liuwallpapermanager.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.liutong.liuwallpapermanager.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 public class WallpaperInfoAdapter extends BaseAdapter {
     private Context context;
@@ -37,6 +42,27 @@ public class WallpaperInfoAdapter extends BaseAdapter {
 
     @Override         
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(context).inflate(R.layout.layout,parent,false);
+            holder.imageView = convertView.findViewById(R.id.service_icon);
+            holder.textView = convertView.findViewById(R.id.service_name);
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        HashMap map = (HashMap) getItem(position);
+
+        holder.textView.setText((CharSequence) map.get("name"));
+        holder.imageView.setImageDrawable((Drawable) map.get("icon"));
+
+        return convertView;
+    }
+
+    public class ViewHolder {
+        private ImageView imageView;
+        private TextView textView;
     }
 }
